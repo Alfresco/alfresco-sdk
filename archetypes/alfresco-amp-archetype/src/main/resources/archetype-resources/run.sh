8fe1d265f8c6d1d9e0e2af46bc3bc5ec26f7fccf
mindthegab <gabriele@apache.org>
2014-09-09 02:34:08 -0400

    -- fixed broken run.sh re-releasing

--- archetypes/alfresco-amp-archetype/src/main/resources/archetype-resources/run.sh
+++ archetypes/alfresco-amp-archetype/src/main/resources/archetype-resources/run.sh
@@ -6,4 +6,4 @@ if [ ! -f $springloadedfile ]; then
 mvn validate -Psetup
 fi
 
-MAVEN_OPTS="-javaagent:$springloadedfile -noverify -Xms256m -Xmx2G -XX:PermSize=300m" mvn integration-test -Prun
+MAVEN_OPTS="-javaagent:$springloadedfile -noverify -Xms256m -Xmx2G -XX:PermSize=300m" mvn integration-test -Pamp-to-war
