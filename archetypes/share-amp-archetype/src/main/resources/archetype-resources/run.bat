9b5db765779ee26b507e242915b4754ef096da5e
Martin Bergljung <martin.bergljung@alfresco.com>
2016-03-03 15:59:22 +0000

    Fix for share-amp/run.bat

--- archetypes/share-amp-archetype/src/main/resources/archetype-resources/run.bat
+++ archetypes/share-amp-archetype/src/main/resources/archetype-resources/run.bat
@@ -1,17 +1,3 @@
 @ECHO OFF
 
-IF "%MAVEN_OPTS%" == "" (
-    ECHO The environment variable 'MAVEN_OPTS' is not set, setting it for you
-
-    SET springloadedfile=%HOME%\.m2\repository\org\springframework\springloaded\1.2.5.RELEASE\springloaded-1.2.5.RELEASE.jar
-
-    if not exist %springloadedfile% (
-      mvn validate -Psetup
-    )
-
-    :: Spring loaded can be used with the Share AMP project in 5.1
-    :: (i.e. it does not have the same problem as Repo AMP and AIO)
-    SET MAVEN_OPTS=-javaagent:"%springloadedfile%" -noverify
-)
-ECHO MAVEN_OPTS is set to '%MAVEN_OPTS%'
 mvn clean install -Pamp-to-war
