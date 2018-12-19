eae6c13de59cf860a0c1c9c590596fb5cae74b0e
Jose Luis Osorno <joseluis.osorno@ixxus.com>
2018-12-19 15:35:04 +0100

    Share archetype - Switch to docker environment
    Change the platform project to add the required configuration to execute it
    as an orchestrated docker environment that includes ACS (optional), Share,
    PostgreSQL and ASS. This will replace the use of the Alfresco Maven Plugin.
    
    New bash scripts has been added to manage the docker environment.
    A new docker-compose file configure the docker environment.
    All the unnecessary files and configuration have been removed from the archetype.

--- archetypes/alfresco-share-jar-archetype/src/main/resources/archetype-resources/run.bat
+++ archetypes/alfresco-share-jar-archetype/src/main/resources/archetype-resources/run.bat
@@ -1,9 +1,91 @@
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::      Dev environment startup script for Alfresco Community.    ::
-::                                                                ::
-::      Note. requires Alfresco.war to be running in another      ::
-::      Tomcat on port 8080.                                      ::
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-@echo off
+#set( $symbol_dollar = '$' )
+@ECHO OFF
 
-mvn clean install alfresco:run
+SET COMPOSE_FILE_PATH=%CD%\target\classes\docker\docker-compose.yml
+
+IF [%1]==[] (
+    echo "Usage: %0 {build_start|start|stop|purge|tail|reload_share|build_test|test}"
+    GOTO END
+)
+
+IF %1==build_start (
+    CALL :down
+    CALL :build
+    CALL :start
+    CALL :tail
+    GOTO END
+)
+IF %1==start (
+    CALL :start
+    CALL :tail
+    GOTO END
+)
+IF %1==stop (
+    CALL :down
+    GOTO END
+)
+IF %1==purge (
+    CALL:down
+    CALL:purge
+    GOTO END
+)
+IF %1==tail (
+    CALL :tail
+    GOTO END
+)
+IF %1==reload_share (
+    CALL :build_share
+    CALL :start_share
+    CALL :tail
+    GOTO END
+)
+IF %1==build_test (
+    CALL :down
+    CALL :build
+    CALL :start
+    CALL :test
+    CALL :down
+    GOTO END
+)
+IF %1==test (
+    CALL :test
+    GOTO END
+)
+echo "Usage: %0 {build_start|start|stop|purge|tail|reload_share|build_test|test}"
+:END
+EXIT /B %ERRORLEVEL%
+
+:start
+    docker volume create alf-acs-volume
+    docker volume create alf-db-volume
+    docker volume create alf-ass-volume
+    docker-compose -f "%COMPOSE_FILE_PATH%" up --build -d
+EXIT /B 0
+:start_share
+    docker-compose -f "%COMPOSE_FILE_PATH%" up --build -d ${rootArtifactId}-share
+EXIT /B 0
+:down
+    docker-compose -f "%COMPOSE_FILE_PATH%" down
+EXIT /B 0
+:build
+    docker rmi alfresco-content-services-${rootArtifactId}:development
+    docker rmi alfresco-share-${rootArtifactId}:development
+	call mvn clean install -DskipTests
+EXIT /B 0
+:build_share
+    docker-compose -f "%COMPOSE_FILE_PATH%" kill ${rootArtifactId}-share
+    docker-compose -f "%COMPOSE_FILE_PATH%" rm -f ${rootArtifactId}-share
+    docker rmi alfresco-share-${rootArtifactId}:development
+	call mvn clean install -DskipTests
+EXIT /B 0
+:tail
+    docker-compose -f "%COMPOSE_FILE_PATH%" logs -f
+EXIT /B 0
+:test
+    call mvn verify -pl
+EXIT /B 0
+:purge
+    docker volume rm alf-acs-volume
+    docker volume rm alf-db-volume
+    docker volume rm alf-ass-volume
+EXIT /B 0
