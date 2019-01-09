4722c1412120cedd881b08cc68440edba5a4e138
Jose Luis Osorno <joseluis.osorno@ixxus.com>
2019-01-09 17:44:18 +0100

    Archetypes - Add logs to archetype ITs
    Add more logs with the result of the execution of the projects
    generation and building in the archetype ITs.

--- archetypes/archetypes-it/src/test/java/org/alfresco/maven/archetype/PlatformJarArchetypeIT.java
+++ archetypes/archetypes-it/src/test/java/org/alfresco/maven/archetype/PlatformJarArchetypeIT.java
@@ -2,11 +2,15 @@ package org.alfresco.maven.archetype;
 
 import org.apache.maven.it.Verifier;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Integration tests for the platform jar archetype.
  */
 public class PlatformJarArchetypeIT extends AbstractArchetypeIT {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PlatformJarArchetypeIT.class);
 
     @Override
     protected ArchetypeProperties createArchetypeProperties() {
@@ -22,12 +26,17 @@ public class PlatformJarArchetypeIT extends AbstractArchetypeIT {
 
     @Test
     public void whenGenerateProjectFromArchetypeThenAProperProjectIsCreated() throws Exception {
-        generateProjectFromArchetype();
+        generateProjectFromArchetype(LOGGER);
+
+        LOGGER.info("---------------------------------------------------------------------");
+        LOGGER.info("Building the generated project {}", archetypeProperties.getProjectArtifactId());
+        LOGGER.info("---------------------------------------------------------------------");
 
         // Since creating the archetype was successful, we now want to actually build the generated project
         Verifier verifier = new Verifier(projectPath);
         verifier.setAutoclean(false);
         verifier.executeGoal("install");
+        printVerifierLog("PROJECT BUILD", verifier, LOGGER);
         verifier.verifyErrorFreeLog();
     }
 }
