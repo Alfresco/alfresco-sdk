ea4c12a83669ea1e57cd799d564e43b1b47f10d8
Jose Luis Osorno <joseluis.osorno@ixxus.com>
2019-03-26 12:25:07 +0100

    Archetypes - Change platform archetype build approach
    - Build the project using package goal instead of install
    - Copy the test dependencies only when IT is executed
    - Add the build_start_it_supported task to execution scripts

--- archetypes/alfresco-platform-jar-archetype/src/main/resources/archetype-resources/run.bat
+++ archetypes/alfresco-platform-jar-archetype/src/main/resources/archetype-resources/run.bat
@@ -12,13 +12,21 @@ IF NOT [%M2_HOME%]==[] (
 )
 
 IF [%1]==[] (
-    echo "Usage: %0 {build_start|start|stop|purge|tail|build_test|test}"
+    echo "Usage: %0 {build_start|build_start_it_supported|start|stop|purge|tail|build_test|test}"
     GOTO END
 )
 
 IF %1==build_start (
     CALL :down
     CALL :build
+    CALL :start
+    CALL :tail
+    GOTO END
+)
+IF %1==build_start_it_supported (
+    CALL :down
+    CALL :build
+    CALL :prepare_test
     CALL :start
     CALL :tail
     GOTO END
@@ -44,6 +52,7 @@ IF %1==tail (
 IF %1==build_test (
     CALL :down
     CALL :build
+    CALL :prepare_test
     CALL :start
     CALL :test
     CALL :tail_all
@@ -70,13 +79,16 @@ EXIT /B 0
     )
 EXIT /B 0
 :build
-	call %MVN_EXEC% clean install -DskipTests
+	call %MVN_EXEC% clean package
 EXIT /B 0
 :tail
     docker-compose -f "%COMPOSE_FILE_PATH%" logs -f
 EXIT /B 0
 :tail_all
     docker-compose -f "%COMPOSE_FILE_PATH%" logs --tail="all"
+EXIT /B 0
+:prepare_test
+    call %MVN_EXEC% verify -DskipTests=true
 EXIT /B 0
 :test
     call %MVN_EXEC% verify
